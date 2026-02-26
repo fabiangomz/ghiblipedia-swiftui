21c6e3248ff3f647831f8a6b9a91832cf15a338d
Fabián Gómez Campo <fabian.campo@icloud.com>
2026-02-26 17:16:43 +0100

    Initial Commit

---
+++ ghiblipedia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ghiblipedia
+//
+//  Created by Fabián Gómez Campo on 26/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ghiblipedia/ghiblipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghiblipediaApp.swift
+//  ghiblipedia
+//
+//  Created by Fabián Gómez Campo on 26/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ghiblipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
