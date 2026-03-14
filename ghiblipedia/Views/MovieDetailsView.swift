30bfc94f70ac1d97ee585240a29ba814fa93cd2d
Fabián Gómez Campo <fabian.campo@icloud.com>
2026-03-14 18:13:07 +0100

    feat: moviedetailsview init

---
+++ ghiblipedia/Views/MovieDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailsView.swift
+//  ghiblipedia
+//
+//  Created by Fabián Gómez Campo on 14/3/26.
+//
+
+import SwiftUI
+
+struct MovieDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailsView()
+}
