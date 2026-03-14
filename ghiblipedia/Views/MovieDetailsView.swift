bcc82b10ee20aa33fa35646529c6c07095b57759
Fabián Gómez Campo <fabian.campo@icloud.com>
2026-03-14 18:17:38 +0100

    feat: design

--- ghiblipedia/Views/MovieDetailsView.swift
+++ ghiblipedia/Views/MovieDetailsView.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct MovieDetailsView: View {
+    
+    let movie: Movie
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            AsyncImage(url: URL(string: movie.movieBanner)) {
+                image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(height: 200)
+            .clipped()
+            
+            HStack {
+                AsyncImage(url: URL(string: movie.image)) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 100, height: 150)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(movie.title)
+                        .font(.title2)
+                        .bold()
+                    Text(movie.description)
+                        .font(.headline)
+                        .foregroundStyle(.secondary)
+                    Text(movie.originalTitleRomanised)
+                        .font(.subheadline)
+                        .foregroundStyle(.tertiary)
+                    Text(movie.releaseDate)
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .padding(.horizontal)
+            
+                      
+            
+        }
     }
 }
 
 #Preview {
-    MovieDetailsView()
+    
+    let movie = Movie(id: "1", title: "Title", originalTitle: "Original title", originalTitleRomanised: "Original title romanji", description: "Desc", director: "Director", producer: "Producer", releaseDate: "Release Date", score: "3", duration: "120", image: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg", movieBanner: "https://image.tmdb.org/t/p/original/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg", people: [])
+    
+    MovieDetailsView(movie: movie)
 }
